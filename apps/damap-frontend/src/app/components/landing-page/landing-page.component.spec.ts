fe7b2efbd45e53d97294f0193bc9f461eaf8c17b
ValentinFutterer <e11904654@student.tuwien.ac.at>
2026-04-15 12:34:05 +0200

    Styles: Fix bug where typography classes werent generated

--- apps/damap-frontend/src/app/components/landing-page/landing-page.component.spec.ts
+++ apps/damap-frontend/src/app/components/landing-page/landing-page.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ActivatedRoute } from '@angular/router';
 import { LandingPageComponent } from './landing-page.component';
-import { AuthService, TranslateTestingModule } from '@damap/core';
+import { TranslateTestingModule } from '@damap/core';
 import { of } from 'rxjs';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { provideHttpClient } from '@angular/common/http';
