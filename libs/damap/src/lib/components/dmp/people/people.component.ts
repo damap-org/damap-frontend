95e2de1ec9bd7935e06afb63863d5df9123c70b4
Vlad Dancea <danceavlad@proton.me>
2026-06-10 02:52:59 +0200

    fix translations

--- libs/damap/src/lib/components/dmp/people/people.component.ts
+++ libs/damap/src/lib/components/dmp/people/people.component.ts
@@ -56,7 +56,6 @@ import { OrcidComponent } from '../../../widgets/orcid/orcid.component';
 import { InputWrapperComponent } from '../../../shared/input-wrapper/input-wrapper.component';
 import { AsyncPipe, KeyValuePipe } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
-import { TranslatePipeMock } from '../../../testing/translate-testing/translate-testing.module';
 import { CdkScrollable } from '@angular/cdk/scrolling';
 
 @Component({
@@ -87,7 +86,6 @@ import { CdkScrollable } from '@angular/cdk/scrolling';
     KeyValuePipe,
     TranslateModule,
     ContributorFilterPipe,
-    TranslatePipeMock,
   ],
 })
 export class PeopleComponent implements OnInit, OnDestroy {
@@ -316,7 +314,6 @@ export class PeopleComponent implements OnInit, OnDestroy {
     MatButton,
     MatDialogClose,
     TranslateModule,
-    TranslatePipeMock,
   ],
 })
 export class ConfirmDeletionDialogComponent {
